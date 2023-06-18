9307d12d4c510d93d502481f15fdefce26070e40
t10o <dev@t10o.one>
2023-06-18 10:26:40 +0900

    :sparkles: supabaseを追加

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
+import { Database } from "@/types/schema";
+
+export const supabase = createClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
