bd27d0216cc746d607e2af93292e25a23dbd7c49
t10o <dev@t10o.one>
2023-06-24 16:11:48 +0900

    :sparkles: サインアウト機能を追加

--- src/components/elements/Menu/Menu.tsx
+++ src/components/elements/Menu/Menu.tsx
@@ -1,11 +1,15 @@
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { createPagesBrowserClient } from "@supabase/auth-helpers-nextjs";
 import clsx from "clsx";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 import { Props, slide as SlideMenu } from "react-burger-menu";
 
+import { Button } from "@/components/elements";
 import { MENUS } from "@/constants/menus";
+import { Database } from "@/types/schema";
 
 interface MenuProps extends Props {
   onClick: () => void;
@@ -13,6 +17,17 @@ interface MenuProps extends Props {
 }
 
 export const Menu = ({ onClick, ...props }: MenuProps) => {
+  const supabase = createPagesBrowserClient<Database>();
+
+  const router = useRouter();
+
+  const handleClick = async () => {
+    await supabase.auth.signOut();
+
+    props.onClose();
+    await router.push("/");
+  };
+
   return (
     <SlideMenu {...props}>
       <div className={clsx("!flex", "justify-end", "items-center")}>
@@ -35,6 +50,14 @@ export const Menu = ({ onClick, ...props }: MenuProps) => {
             </li>
           );
         })}
+
+        <li className={clsx("my-8")}>
+          <Button
+            className={clsx("text-2xl", "pl-0")}
+            label="Sign Out"
+            onClick={handleClick}
+          />
+        </li>
       </ul>
     </SlideMenu>
   );
