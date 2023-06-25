235b54ef7067958bb33174d962f689f430c631e4
t10o <dev@t10o.one>
2023-06-25 23:04:19 +0900

    :+1: Footerのメニューを削除

--- src/components/layouts/Footer/Footer.tsx
+++ src/components/layouts/Footer/Footer.tsx
@@ -1,26 +1,9 @@
 import clsx from "clsx";
-import Link from "next/link";
-
-import { MENUS } from "@/constants/menus";
 
 export const Footer = (): JSX.Element => {
   return (
-    <footer className={clsx("text-center", "mt-40", "p-16")}>
-      <div className={clsx("flex", "items-center", "justify-center")}>
-        {MENUS.map((menu) => {
-          return (
-            <Link
-              className={clsx("mx-4", "text-xl")}
-              key={menu.name}
-              href={menu.href}
-            >
-              {menu.name}
-            </Link>
-          );
-        })}
-      </div>
-
-      <p className={clsx("mt-16")}>© t10o</p>
+    <footer className={clsx("text-center", "p-16", "mt-16")}>
+      <p>© t10o</p>
     </footer>
   );
 };
